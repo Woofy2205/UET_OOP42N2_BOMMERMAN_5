9848a925cc2372d306d318b2034a2ee2aff946dd
Nguyễn Duy Chiến <100187821+Woofy2205@users.noreply.github.com>
2022-09-28 11:58:34 +0700

    GameManager finishing and changing from HelloApplication to MainGame
    GameManager still using default constructor (haven't loaded the sprites yet because we haven't got the sprites)

--- src/main/java/bomman/MainGame.java
+++ src/main/java/bomman/MainGame.java
@@ -1,7 +1,25 @@
 package bomman;
 
-/**
- * This class will execute the game.
- */
-public class MainGame {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static bomman.manager.GameManager.GAME_TITLE;
+
+public class MainGame extends Application {
+	@Override
+	public void start(Stage stage) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(MainGame.class.getResource("hello-view.fxml"));
+		Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+		stage.setTitle(GAME_TITLE);
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch();
+	}
 }
--- src/main/java/bomman/Manager/SoundManager.java
+++ src/main/java/bomman/Manager/SoundManager.java
@@ -4,4 +4,5 @@ package bomman.manager;
  * This class will manage all the sounds in the game.
  */
 public class SoundManager {
+
 }
