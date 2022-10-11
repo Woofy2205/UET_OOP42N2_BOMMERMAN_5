6a584865f4b87559ab0de0303b688099cbd4e7c2
anhensam4 <89511405+anhensam4@users.noreply.github.com>
2022-10-11 23:56:41 +0700

    player movement demo 1

--- src/main/java/bomman/MainGame.java
+++ src/main/java/bomman/MainGame.java
@@ -1,91 +1,132 @@
 package bomman;
 
+import bomman.entity.CommonEntity;
 import bomman.entity.MainCharacter;
 import bomman.entity.tiles.HiddenTiles;
 import bomman.entity.tiles.UnbreakableTiles;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import bomman.manager.Sprite;
 import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
-
-import bomman.entity.CommonEntity;
-import bomman.manager.Sprite;
-
 public class MainGame extends Application {
-	public static final int WIDTH = 20;
-	public static final int HEIGHT = 15;
+    static Scene mainScene;
+    public static final int WIDTH = 20;
+    public static final int HEIGHT = 15;
 
-	private GraphicsContext gc;
-	private Canvas canvas;
-	private List<CommonEntity> entities = new ArrayList<CommonEntity>();
-	private List<CommonEntity> stillObjects = new ArrayList<>();
+    private GraphicsContext gc;
+    private Canvas canvas;
+    private List<CommonEntity> entities = new ArrayList<CommonEntity>();
+    private List<CommonEntity> stillObjects = new ArrayList<>();
+
+    private static CommonEntity bomberman;
+
+    static HashSet<String> currentlyActiveKeys;
+
+    final long startNanoTime = System.nanoTime();
 
 
-	public static void main(String[] args) {
-		launch();
-	}
+    public static void main(String[] args) {
+        launch();
+    }
 
-	@Override
-	public void start(Stage stage) {
-		// Tao Canvas
-		canvas = new Canvas(Sprite.SCALED_SIZE * WIDTH, Sprite.SCALED_SIZE * HEIGHT);
-		gc = canvas.getGraphicsContext2D();
+    @Override
+    public void start(Stage stage) {
+        // Tao Canvas
+        canvas = new Canvas(Sprite.SCALED_SIZE * WIDTH, Sprite.SCALED_SIZE * HEIGHT);
+        gc = canvas.getGraphicsContext2D();
 
-		// Tao root container
-		Group root = new Group();
-		root.getChildren().add(canvas);
+        // Tao root container
+        Group root = new Group();
+        root.getChildren().add(canvas);
 
-		// Tao scene
-		Scene scene = new Scene(root);
+        // Tao scene
+        mainScene = new Scene(root);
+        prepareActionHandlers();
 
-		// Them scene vao stage
-		stage.setScene(scene);
-		stage.show();
+        // Them scene vao stage
+        stage.setScene(mainScene);
+        stage.show();
 
-		AnimationTimer timer = new AnimationTimer() {
-			@Override
-			public void handle(long l) {
-				render();
-				update();
-			}
-		};
-		timer.start();
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                tickAndRender();
+                render();
+                update();
+            }
+        };
+        timer.start();
 
-		createMap();
+        createMap();
 
-		CommonEntity bomberman = new MainCharacter(1, 1, Sprite.player_right.getFxImage());
-		entities.add(bomberman);
-	}
+        bomberman = new MainCharacter(1, 1, Sprite.player_right.getFxImage());
+        entities.add(bomberman);
+    }
 
-	public void createMap() {
-		for (int i = 0; i < WIDTH; i++) {
-			for (int j = 0; j < HEIGHT; j++) {
-				CommonEntity object;
-				if (j == 0 || j == HEIGHT - 1 || i == 0 || i == WIDTH - 1) {
-					object = new UnbreakableTiles(i, j, Sprite.wall.getFxImage());
-				}
-				else {
-					object = new HiddenTiles(i, j, Sprite.grass.getFxImage());
-				}
-				stillObjects.add(object);
-			}
-		}
-	}
+    public void createMap() {
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                CommonEntity object;
+                if (j == 0 || j == HEIGHT - 1 || i == 0 || i == WIDTH - 1) {
+                    object = new UnbreakableTiles(i, j, Sprite.wall.getFxImage());
+                } else {
+                    object = new HiddenTiles(i, j, Sprite.grass.getFxImage());
+                }
+                stillObjects.add(object);
+            }
+        }
+    }
 
-	public void update() {
-		entities.forEach(CommonEntity::update);
-	}
+    public void update() {
+        entities.forEach(CommonEntity::update);
+    }
 
-	public void render() {
-		gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-		stillObjects.forEach(g -> g.render(gc));
-		entities.forEach(g -> g.render(gc));
-	}
+    public void render() {
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        stillObjects.forEach(g -> g.render(gc));
+        entities.forEach(g -> g.render(gc));
+    }
+
+    private static void prepareActionHandlers() {
+        // use a set so duplicates are not possible
+        currentlyActiveKeys = new HashSet<String>();
+        mainScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                currentlyActiveKeys.add(event.getCode().toString());
+            }
+        });
+        mainScene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                currentlyActiveKeys.remove(event.getCode().toString());
+            }
+        });
+    }
+
+    private static void tickAndRender() {
+        if (currentlyActiveKeys.contains("LEFT")) {
+            bomberman.move(CommonEntity.DIRECTION.LEFT);
+        }
+        if (currentlyActiveKeys.contains("RIGHT")) {
+            bomberman.move(CommonEntity.DIRECTION.RIGHT);
+        }
+        if (currentlyActiveKeys.contains("UP")) {
+            bomberman.move(CommonEntity.DIRECTION.UP);
+        }
+        if (currentlyActiveKeys.contains("DOWN")) {
+            bomberman.move(CommonEntity.DIRECTION.DOWN);
+        }
+    }
 }
