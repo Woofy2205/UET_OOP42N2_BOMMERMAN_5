59c27bc6b5c153965dfe53f2ae7a2c9fbba48906
Nguyễn Duy Chiến <100187821+Woofy2205@users.noreply.github.com>
2022-10-12 00:32:29 +0700

    Conflict solving and Merge Chien's branch to An's branch
    Conflict causing because of MainGame's adjustments and other related stuffs, now fixed

--- src/main/java/bomman/MainGame.java
+++ src/main/java/bomman/MainGame.java
@@ -1,9 +1,11 @@
 package bomman;
 
 import bomman.entity.CommonEntity;
+import bomman.entity.EntityManager;
 import bomman.entity.MainCharacter;
 import bomman.entity.tiles.HiddenTiles;
 import bomman.entity.tiles.UnbreakableTiles;
+import bomman.manager.GameManager;
 import bomman.manager.Sprite;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
@@ -20,16 +22,21 @@ import java.util.HashSet;
 import java.util.List;
 
 public class MainGame extends Application {
+
+    // gameManager to control the game, create map, entities and other stuffs.
+    GameManager gameManager = new GameManager();
+    static EntityManager entityManager = new EntityManager();
+
     static Scene mainScene;
     public static final int WIDTH = 20;
     public static final int HEIGHT = 15;
 
     private GraphicsContext gc;
     private Canvas canvas;
-    private List<CommonEntity> entities = new ArrayList<CommonEntity>();
-    private List<CommonEntity> stillObjects = new ArrayList<>();
+    //private List<CommonEntity> entities = new ArrayList<CommonEntity>();
+    //private List<CommonEntity> stillObjects = new ArrayList<>();
 
-    private static CommonEntity bomberman;
+//  private static CommonEntity bomberman;
 
     static HashSet<String> currentlyActiveKeys;
 
@@ -68,34 +75,33 @@ public class MainGame extends Application {
         };
         timer.start();
 
-        createMap();
-
-        bomberman = new MainCharacter(1, 1, Sprite.player_right.getFxImage());
-        entities.add(bomberman);
+//      createMap();
+        gameManager.createMapFromFile();
+        entityManager.createEntity();
     }
 
-    public void createMap() {
-        for (int i = 0; i < WIDTH; i++) {
-            for (int j = 0; j < HEIGHT; j++) {
-                CommonEntity object;
-                if (j == 0 || j == HEIGHT - 1 || i == 0 || i == WIDTH - 1) {
-                    object = new UnbreakableTiles(i, j, Sprite.wall.getFxImage());
-                } else {
-                    object = new HiddenTiles(i, j, Sprite.grass.getFxImage());
-                }
-                stillObjects.add(object);
-            }
-        }
-    }
+//    public void createMap() {
+//        for (int i = 0; i < WIDTH; i++) {
+//            for (int j = 0; j < HEIGHT; j++) {
+//                CommonEntity object;
+//                if (j == 0 || j == HEIGHT - 1 || i == 0 || i == WIDTH - 1) {
+//                    object = new UnbreakableTiles(i, j, Sprite.wall.getFxImage());
+//                } else {
+//                    object = new HiddenTiles(i, j, Sprite.grass.getFxImage());
+//                }
+//                gameManager.stillObjects.add(object);
+//            }
+//        }
+//    }
 
     public void update() {
-        entities.forEach(CommonEntity::update);
+        entityManager.entities.forEach(CommonEntity::update);
     }
 
     public void render() {
         gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-        stillObjects.forEach(g -> g.render(gc));
-        entities.forEach(g -> g.render(gc));
+        gameManager.stillObjects.forEach(g -> g.render(gc));
+        entityManager.entities.forEach(g -> g.render(gc));
     }
 
     private static void prepareActionHandlers() {
@@ -117,16 +123,16 @@ public class MainGame extends Application {
 
     private static void tickAndRender() {
         if (currentlyActiveKeys.contains("LEFT")) {
-            bomberman.move(CommonEntity.DIRECTION.LEFT);
+            entityManager.bomberman.move(CommonEntity.DIRECTION.LEFT);
         }
         if (currentlyActiveKeys.contains("RIGHT")) {
-            bomberman.move(CommonEntity.DIRECTION.RIGHT);
+            entityManager.bomberman.move(CommonEntity.DIRECTION.RIGHT);
         }
         if (currentlyActiveKeys.contains("UP")) {
-            bomberman.move(CommonEntity.DIRECTION.UP);
+            entityManager.bomberman.move(CommonEntity.DIRECTION.UP);
         }
         if (currentlyActiveKeys.contains("DOWN")) {
-            bomberman.move(CommonEntity.DIRECTION.DOWN);
+            entityManager.bomberman.move(CommonEntity.DIRECTION.DOWN);
         }
     }
 }
--- src/main/java/bomman/entity/EntityManager.java
+++ src/main/java/bomman/entity/EntityManager.java
@@ -1,9 +1,28 @@
 package bomman.entity;
 
+import bomman.manager.Sprite;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * This class will manage all the entities' action.
  * Add, Remove, ...
  */
 public class EntityManager {
+
+	public static List<CommonEntity> entities = new ArrayList<CommonEntity>();
+	public static CommonEntity bomberman;
+
+	public EntityManager() {
+		// entities = new ArrayList<CommonEntity>();
+	}
+
+	public static void createEntity() {
+		bomberman = new MainCharacter(1, 1, Sprite.player_right.getFxImage());
+		CommonEntity bomb = new Bomb(1,2, Sprite.bomb.getFxImage(), 100);
+		entities.add(bomberman);
+		entities.add(bomb);
+	}
 
 }
